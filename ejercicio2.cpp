241516bc161483f8b3054f029e6af28b9dc1909c
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-06-23 23:11:29 -0500

    Remplazada la operacion por la funcion esPesi

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void esPesi(int n1,int n2);
+
 int main(){
     int n1,n2;
-    int c=0;
-    int i=1;
+
+    cout<<"Ingrese dos numeros: \n";
     cin>>n1>>n2;
+
+    //llamado a la funcion esPesi
+    esPesi(n1,n2);
+    return 0;
+}
+
+void esPesi(int n1, int n2){
+    int c=0;  
+    int i=1; //iterador 
+
     while(i<=n1 && i<=n2){
         if(n1%i==0 && n2%i==0){
-            c=c+i;
+        //si se cumple la condicion va acumulando la cantidad de divisores
+            c=c+i; 
         }
-        i=i+1;
+        i=i+1; 
     }
-    if(c>1){
+
+    if(c>1){ //si tienen mas de un divisor en comun 
         cout<<"No son PESI";
     }else{
         cout<<"Son PESI";
     }
-    return 0;
 }
